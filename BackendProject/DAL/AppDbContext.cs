c8a6a5fd10bc622b3e1e40882e75267c8c9bff00
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-28 15:10:29 +0400

    feature:courseDetail entity created

--- BackendProject/DAL/AppDbContext.cs
+++ BackendProject/DAL/AppDbContext.cs
@@ -8,8 +8,14 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
-
+    //home
      public DbSet<Slider>Sliders { get; set; }
      public DbSet<SliderDetail>SliderDetail { get; set; }
      public DbSet<Notice>Notices { get; set; }
+     public DbSet<Info> info { get; set; }
+
+     //course
+     public DbSet<Category> categories  { get; set; }
+     public DbSet<Course> courses { get; set; }
+
 }
--- BackendProject/Models/Course/Course.cs
+++ BackendProject/Models/Course/Course.cs
@@ -9,4 +9,6 @@ public class Course
     public string ImageUrl { get; set; }
     public string CategoryId { get; set; }
     public Category Category { get; set; }        
+    public string CourseDetail { get; set; }
+    public CourseDetail courseDetail { get; set; }
 }
