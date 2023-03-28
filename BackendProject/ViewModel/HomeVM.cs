687f47371f2cc35323b3c5f70bc4e925dd7366c1
Asgar Y. Asgarov <asgaryas@code.local>
2023-03-28 20:59:50 +0400

    feature:HomeVM added

---
+++ BackendProject/ViewModel/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using BackendProject.Models;
+using System.Collections.Generic;
+
+namespace BackendProject.ViewModel
+{
+    public class HomeVM
+    {
+        public List<Slider>  Sliders { get; set; }
+
+    }
+}
