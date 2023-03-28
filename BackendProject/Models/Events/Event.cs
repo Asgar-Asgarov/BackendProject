d3c790302b531d5bc92713dd518b9cb6deeb1fc8
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-28 15:31:01 +0400

    feature:thought entity created

--- BackendProject/Models/Events/Event.cs
+++ BackendProject/Models/Events/Event.cs
@@ -5,7 +5,7 @@ public class Event
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string  Place { get; set; }
+    public string?  Place { get; set; }
     public DateOnly date { get; set; }
     public DateTime time { get; set; }
     
---
+++ BackendProject/Models/Home/Thought.cs
@@ -0,0 +1,9 @@
+  namespace BackendProject.Models;
+
+  public class Thought
+  {
+    public int Id { get; set; }
+    public string?  ImageUrl { get; set; }
+    public string? Description { get; set; }
+    public string? Profession { get; set; }
+  }
