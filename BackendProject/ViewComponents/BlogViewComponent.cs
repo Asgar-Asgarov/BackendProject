2ba42f2e8d94e1f49a2a3befac2d653e690d282b
Asgar Y. Asgarov <asgaryas@code.local>
2023-03-28 20:24:06 +0400

    feature:BlogViewComponent added

---
+++ BackendProject/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.ViewComponents
+{
+    public class BlogViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+
+            return View();
+        }
+    }
+}
