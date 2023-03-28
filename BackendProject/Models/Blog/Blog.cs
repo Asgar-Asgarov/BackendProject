21146ab261266cac879b628ed07594ea7f58ab35
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-28 15:34:06 +0400

    feature:blog entity created

---
+++ BackendProject/Models/Blog/Blog.cs
@@ -0,0 +1,11 @@
+
+namespace   BackendProject.Models;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime time { get; set; }
+    //comment count property
+}
