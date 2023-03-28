d1b256865ced4967604d2a07eca0adee9c9b87b2
Asgar Y. Asgarov <asgaryas@code.local>
2023-03-28 20:29:53 +0400

    feature:Thought ViewComponent added

---
+++ BackendProject/ViewComponents/ThoughtViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.ViewComponents
+{
+    public class ThoughtViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+
+            return View();
+        }
+    }
+}
