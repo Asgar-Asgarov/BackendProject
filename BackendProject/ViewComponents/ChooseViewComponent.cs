2bd6537296c58d14419db97dd240499fa0ae690e
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-04-03 17:29:03 +0400

    feature:info reflected from database

--- BackendProject/ViewComponents/ChooseViewComponent.cs
+++ BackendProject/ViewComponents/ChooseViewComponent.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BackendProject.DAL;
+using BackendProject.ViewModel;
 
 namespace BackendProject.ViewComponents
 {
     public class ChooseViewComponent: ViewComponent
     {
+             private readonly AppDbContext _appDbContext;
+
+        public ChooseViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-
-
-            return View();
+           HomeVM homeVM = new HomeVM();
+           homeVM.info=_appDbContext.info.FirstOrDefault();
+            return View(await Task.FromResult(homeVM));
         }
 
     }
--- BackendProject/ViewModel/HomeVM.cs
+++ BackendProject/ViewModel/HomeVM.cs
@@ -8,6 +8,7 @@ namespace BackendProject.ViewModel
         public List<Slider>?  Sliders { get; set; }
         public SliderDetail? SliderDetail { get; set; }
         public List<Notice>? Notices { get; set; }
+        public Info? info { get; set; }
 
     }
 }
