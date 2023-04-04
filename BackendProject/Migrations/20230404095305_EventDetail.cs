a2dd0c2a803a7a1f820239cf8b0e00c0edb23658
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-04-04 13:57:24 +0400

    feature:eventDetail migrations added

---
+++ BackendProject/Migrations/20230404095305_EventDetail.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "finishTime",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "startTime",
+                table: "events");
+
+            migrationBuilder.CreateTable(
+                name: "EventDetail",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Speakers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    startTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    finishTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventDetail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventDetail_events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventDetail_EventId",
+                table: "EventDetail",
+                column: "EventId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventDetail");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date",
+                table: "events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "finishTime",
+                table: "events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "startTime",
+                table: "events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
